3d255597036d7df85c25724f0b10ad983aa514c1
gauravdhadan24 <gauravdhadan24@gmail.com>
2024-03-10 18:01:50 +0530

    search api, header searh , search ui

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.mongo)
   .then(() => {
     console.log('Connected to MongoDB!');
   })
